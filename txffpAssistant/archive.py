652db8cb83a178e4995575fa2fd8a683a37ac780
kyle <kairu_madigan@yahoo.co.jp>
2018-07-25 13:32:11 +0900

    feat(archive): 新增get_zipfile函数
    获取目标目录下的压缩包文件，返回一个包含压缩包文件路径的迭代对象。

--- txffpAssistant/archive.py
+++ txffpAssistant/archive.py
@@ -19,3 +19,18 @@ def is_zip(obj):
     if kind.extension == "zip":
         return True
     return False
+
+
+def get_zipfile(filepath):
+    if os.path.isdir(filepath):
+        for root, dirs, files in os.walk(filepath):
+            for fn in files:
+                fp = os.path.join(root, fn)
+                if is_zip(fp):
+                    yield fp
+    
+    elif os.path.isfile(filepath):
+        if is_zip(filepath):
+            yield filepath
+        else:
+            return iter([])
