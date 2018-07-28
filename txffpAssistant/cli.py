2a512beef1018889500478699af7a734dd1773fa
kyle <kairu_madigan@yahoo.co.jp>
2018-07-28 14:55:30 +0900

    refactor: 重构验证和命令执行
    使用action重写参数验证
    任务执行由函数更改为类，并动态执行

--- txffpAssistant/cli.py
+++ txffpAssistant/cli.py
@@ -8,6 +8,8 @@
 import argparse
 import getpass
 import logging
+import os
+import string
 import sys
 
 from . import exceptions
@@ -24,11 +26,6 @@ logg_level_dict = {
     "warn": logging.WARN,
     "error": logging.ERROR,
     "critical": logging.CRITICAL
-}
-
-etc_type_dict = {
-    "personal": "PERSONAL",
-    "company": "COMPANY"
 }
 
 
@@ -57,19 +54,12 @@ def authstr_parser(authstr:str) -> (str, str):
         username = parsed[0]
         password = parsed[1]
     return username, password
-    
-
-def inv_dl():
-    pass
 
 
-def etc_list(username, password, logger, etc_type="all"):
+def etc_card(username, password, logger, etc_type="all"):
     if etc_type.upper() != "ALL" and etc_type.lower() not in etc_type_dict:
         raise ValueError("etc_type not in {}".format(etc_type_dict.keys()))
-    
-    logger.info("模拟登陆...")
-    authed_session = handler.authenticated_session(
-        username, password, logger=logger)
+
 
     etc_handler = handler.ETCCardHandler(
         session=authed_session, logger=logger)
@@ -96,7 +86,129 @@ def etc_list(username, password, logger, etc_type="all"):
         logger.info("个人卡{}张".format(len(p_etcinfo)))
     elif "c_etcinfo" in local_keys:
         logger.info("单位卡{}张".format(len(c_etcinfo)))
+        
+        
+class MonthAction(argparse.Action):
+    
+    def __call__(self, parser, namespace, values, option_string=None):
+        self.validator(values)
+        setattr(namespace, self.dest, values)
+        
+    def validator(self, yearmonth):
+        if not yearmonth.isdigit() or len(yearmonth) != 6:
+            print("Error: 月份信息不合法，格式：year+month, 例：201805", file=sys.stderr)
+            sys.exit(1)
+        year = yearmonth[:4]
+        month = yearmonth[4:]
+        month_all = [str(m) for m in range(1, 13)]
+        if month not in month_all:
+            print("Error: 月份信息不合法，所指定月份不存在")
+            sys.exit(1)
 
+
+class AuthAction(argparse.Action):
+    
+    def __call__(self, parser, namespace, values, option_string=None):
+        values = self.validator(values)
+        setattr(namespace, self.dest, values)
+        
+    def validator(self, values: str):
+        if values.startswith(":"):
+            print("Error: 认证账户格式错误", file=sys.stderr)
+
+        parsed = values.split(":", 1)
+        if len(parsed) == 1:
+            password = get_password()
+            values = parsed[0] + ":" + password
+        return values
+
+
+class IDAction(argparse.Action):
+    
+    def __call__(self, parser, namespace, values, option_string=None):
+        self.validator(values)
+        setattr(namespace, self.dest, values)
+    
+    @staticmethod
+    def ishexdigit(char):
+        result = (char in string.hexdigits)
+        return result
+        
+    def validator(self, values: str):
+        if len(values) != 32:
+            print("Error: ID信息不合法，长度不等于32", file=sys.stderr)
+            sys.exit(1)
+    
+        resultmap = map(self.ishexdigit, values)
+        if False in resultmap:
+            print("Error: ID信息不合法，存在非十六进制字符")
+            sys.exit(1)
+            
+
+class Service(object):
+    
+    def __init__(self, options, logger):
+        self.options = options
+        self.logger = logger
+        self.username = ""
+        self.password = ""
+        
+    def auth(self):
+        if hasattr(self.options, "auth") and self.options.auth:
+            self.username, self.password = authstr_parser(options.auth)
+        else:
+            self.username, self.password = get_uname_passwd()
+
+    def login(self):
+        self.auth()
+    
+        self.logger.info("模拟登陆...")
+        authed_session = handler.authenticated_session(
+            self.username, self.password, logger=self.logger)
+        return authed_session
+            
+    def run(self):
+        pass
+    
+
+class EtcService(Service):
+    
+    @staticmethod
+    def get_etc_info(handler, etc_type):
+        etc = handler.get_cardlist(etc_type)
+        etcinfo = [ei for ei_iter in etc for ei in ei_iter]
+        return etcinfo
+    
+    def run(self):
+        authed_session = self.login()
+        
+        etc_handler = handler.ETCCardHandler(
+            session=authed_session, logger=self.logger)
+        
+        etc_type = self.options.etc_type.upper()
+        if etc_type == "ALL":
+            p_etcinfo = self.get_etc_info(etc_handler, "PERSONAL")
+            c_etcinfo = self.get_etc_info(etc_handler, "COMPANY")
+        elif etc_type == "PERSONAL":
+            p_etcinfo = self.get_etc_info(etc_handler, "PERSONAL")
+        elif etc_type == "COMPANY":
+            c_etcinfo = self.get_etc_info(etc_handler, "COMPANY")
+
+        self.logger.info("已完成ETC卡信息的获取")
+        local_keys = locals().keys()
+        if "p_etcinfo" in local_keys and "c_etcinfo" in local_keys:
+            self.logger.info("单位卡{}张，个人卡{}张".format(len(c_etcinfo), len(p_etcinfo)))
+        elif "p_etcinfo" in local_keys:
+            self.logger.info("个人卡{}张".format(len(p_etcinfo)))
+        elif "c_etcinfo" in local_keys:
+            self.logger.info("单位卡{}张".format(len(c_etcinfo)))
+        
+        
+class RecordService(Service):
+    
+    def run(self):
+        print(self.options)
+        
 
 def main():
     description = "使用过程中出现问题，请到xxx发起issue。"
@@ -107,10 +219,20 @@ def main():
     service_subparser = parser.add_subparsers(title="Commands", dest="command")
     
     # etc card list
-    service_etc_list = service_subparser.add_parser("etc-list", help="查看ETC卡信息")
-    service_etc_list.add_argument("-t", "--type", dest="etc_type", choices=["personal", "company", "all"],
+    service_etc = service_subparser.add_parser("etc", help="查看ETC卡信息")
+    service_etc.add_argument("--type", dest="etc_type", choices=["personal", "company", "all"],
                                   default="all", help="指定etc卡类型，默认：all")
-    service_etc_list.add_argument("-a", "--auth", dest="auth", type=str, help="票根网用户名和密码")
+    service_etc.add_argument("--auth", action=AuthAction, dest="auth", type=str,
+                                  help="票根网用户名和密码，格式：username:password")
+    
+    # invoice record
+    service_record = service_subparser.add_parser("record" ,help="查看开票记录")
+    service_record.add_argument("--id", action=IDAction ,dest="etc_id", type=str, required=True, help="指定ETC卡id")
+    service_record.add_argument("--month", action=MonthAction, dest="month", type=str, required=True, help="开票年月，例如: 201805")
+    service_record.add_argument("--type", dest="etc_type", choices=["personal", "company"],
+                                  default="conpany", help="指定etc卡类型，默认：personal")
+    service_record.add_argument("--auth", action=AuthAction, dest="auth", type=str, help="票根网用户名和密码，格式：username:password")
+    
     
     # invoice download
     service_inv_dl = service_subparser.add_parser("inv-dl", help="下载发票")
@@ -120,7 +242,7 @@ def main():
         parser.parse_args(["-h"])
     
     options = parser.parse_args()
-    print(options)
+    print("options: {}".format(options))
     
     logger = log.stream_logger()
     # debug mode
@@ -128,28 +250,10 @@ def main():
         logger.setLevel(logging.DEBUG)
         logger.debug("启用debug模式")
 
-    # service etc card list
-    if hasattr(options, "auth") and options.auth:
-        username, password = authstr_parser(options.auth)
-    elif options.command is not None:
-        username, password = get_uname_passwd()
-    
-
-    # logger.info("当前用户：{}".format(username))
-    if options.command == "etc-list":
-        etc_type = options.etc_type
-        try:
-            etc_list(username, password, logger, etc_type)
-        except exceptions.AuthFailedError:
-            sys.exit(1)
-    
-    
-    
-    # if __name__ == "__main__":
-    
-        
-    
-    pass
+    if options.command:
+        class_name = options.command.title() + "Service"
+        service = eval(class_name)(options, logger)
+        service.run()
 
 
 if __name__ == "__main__":
